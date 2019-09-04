df3ed4c83d42f8273af2a923002b6d10397e55da
Brian Choi <sukhoonchoi1@gmail.com>
2019-09-04 16:55:22 +1200

    add starting year month year

--- src/main/java/simulations/Simulator.java
+++ src/main/java/simulations/Simulator.java
@@ -14,16 +14,19 @@ public class Simulator {
     public static void main(String[] args) {
         String filename = "Carpark";
 
-        if (args.length < 7) {
+        if (args.length < 10) {
             System.out.println(
                     "Invalid amount of arguments \n" +
                             "Arg 0: CarPark ID \n" +
                             "Arg 1: CarPark Capacity \n" +
                             "Arg 2: Number of runs \n" +
                             "Arg 3: Delay in minutes between Simulations \n" +
-                            "Arg 4: Starting Hour (24h) \n" +
-                            "Arg 5: Starting Minute \n" +
-                            "Arg 6: Starting Second \n" +
+                            "Arg 4: Starting Day \n" +
+                            "Arg 5: Starting Month \n" +
+                            "Arg 6: Starting Year \n" +
+                            "Arg 7: Starting Hour (24h) \n" +
+                            "Arg 8: Starting Minute \n" +
+                            "Arg 9: Starting Second \n" +
                             "\n" + "e.g. java -jar CarparkSimulator.jar 1 100 288 5 12 30 0"
             );
             System.exit(0);
@@ -34,9 +37,12 @@ public class Simulator {
                 int carParkCapacity = Integer.parseInt(args[1]);
                 int numberOfRuns = Integer.parseInt(args[2]);
                 int delayBetweenMinutes = Integer.parseInt(args[3]);
-                int startingHour = Integer.parseInt(args[4]);
-                int startingMinute = Integer.parseInt(args[5]);
-                int startingSecond = Integer.parseInt(args[6]);
+                int day = Integer.parseInt(args[4]);
+                int month = Integer.parseInt(args[5]);
+                int year = Integer.parseInt(args[6]);;
+                int startingHour = Integer.parseInt(args[7]);
+                int startingMinute = Integer.parseInt(args[8]);
+                int startingSecond = Integer.parseInt(args[9]);
 
                 // Valid argument check
                 if (carParkID < 0 | carParkCapacity < 1 | numberOfRuns < 1 | delayBetweenMinutes < 1
@@ -45,14 +51,8 @@ public class Simulator {
                     throw new IllegalArgumentException("Argument passed has an invalid value");
                 }
 
-                Calendar calendar = GregorianCalendar.getInstance();
-                calendar.set(
-                        calendar.get(Calendar.YEAR),
-                        calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH),
-                        startingHour,
-                        startingMinute,
-                        startingSecond);
+                Calendar calendar = new GregorianCalendar(
+                        year, month-1, day, startingHour, startingMinute, startingSecond);
 
 //                CSVWriter csvWriter = new CSVWriter(filename);
                 JSONWriter jsonWriter = new JSONWriter(filename);
@@ -75,7 +75,7 @@ public class Simulator {
                     System.out.println("Data @" + cp.getTimestampAsString() + " appended");
 
                     try {
-                        Thread.sleep(delayBetweenMinutes * 1000);
+                        Thread.sleep(delayBetweenMinutes * 10);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
